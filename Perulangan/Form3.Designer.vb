0c1406522e8067e83023aa5dff3b7663d167272e
Muhammad Aisamuddin Eka Pramudita <isam.muhammad@ymail.com>
2015-10-17 23:20:16 +0700

    Tugas 3

--- Perulangan/Form3.Designer.vb
+++ Perulangan/Form3.Designer.vb
@@ -22,19 +22,136 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.matriks1 = New System.Windows.Forms.TextBox()
+        Me.hitungButton = New System.Windows.Forms.Button()
+        Me.matriks2 = New System.Windows.Forms.TextBox()
+        Me.operasi = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.hasilText = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'matriks1
+        '
+        Me.matriks1.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.matriks1.Location = New System.Drawing.Point(89, 12)
+        Me.matriks1.Multiline = True
+        Me.matriks1.Name = "matriks1"
+        Me.matriks1.Size = New System.Drawing.Size(340, 75)
+        Me.matriks1.TabIndex = 11
+        '
+        'hitungButton
+        '
+        Me.hitungButton.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hitungButton.Location = New System.Drawing.Point(354, 367)
+        Me.hitungButton.Name = "hitungButton"
+        Me.hitungButton.Size = New System.Drawing.Size(75, 23)
+        Me.hitungButton.TabIndex = 10
+        Me.hitungButton.Text = "Hitung"
+        Me.hitungButton.UseVisualStyleBackColor = True
+        '
+        'matriks2
+        '
+        Me.matriks2.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.matriks2.Location = New System.Drawing.Point(89, 131)
+        Me.matriks2.Multiline = True
+        Me.matriks2.Name = "matriks2"
+        Me.matriks2.Size = New System.Drawing.Size(340, 75)
+        Me.matriks2.TabIndex = 9
+        '
+        'operasi
+        '
+        Me.operasi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.operasi.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.operasi.FormattingEnabled = True
+        Me.operasi.Items.AddRange(New Object() {"Tambah", "Kurang"})
+        Me.operasi.Location = New System.Drawing.Point(89, 97)
+        Me.operasi.Name = "operasi"
+        Me.operasi.Size = New System.Drawing.Size(340, 24)
+        Me.operasi.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 14)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Matriks 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(13, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 14)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Operasi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(13, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 14)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Matriks 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(13, 225)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 14)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Hasil"
+        '
+        'hasilText
+        '
+        Me.hasilText.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.hasilText.Location = New System.Drawing.Point(89, 222)
+        Me.hasilText.Multiline = True
+        Me.hasilText.Name = "hasilText"
+        Me.hasilText.ReadOnly = True
+        Me.hasilText.Size = New System.Drawing.Size(340, 75)
+        Me.hasilText.TabIndex = 13
         '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(441, 402)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.hasilText)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.matriks1)
+        Me.Controls.Add(Me.hitungButton)
+        Me.Controls.Add(Me.matriks2)
+        Me.Controls.Add(Me.operasi)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form3"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Matriks Kalkulator"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents matriks1 As System.Windows.Forms.TextBox
+    Friend WithEvents hitungButton As System.Windows.Forms.Button
+    Friend WithEvents matriks2 As System.Windows.Forms.TextBox
+    Friend WithEvents operasi As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents hasilText As System.Windows.Forms.TextBox
 End Class
--- Perulangan/Form3.vb
+++ Perulangan/Form3.vb
@@ -1,5 +1,107 @@
 ﻿Public Class Form3
 
-    
+    Private Function masukan(ByVal matriks As String) As List(Of List(Of Integer))
+        Dim daftar As New List(Of List(Of Integer))
+        Dim baris As String() = matriks.Split(vbCrLf)
+        Dim panjangBaris As Integer = baris.Length
+        Dim angka As Integer = (panjangBaris - 1)
+        Dim i As Integer = 0
+        Do While (i <= angka)
+            Dim kolom As String() = baris(i).Split(" ")
+            Dim angka2 As Integer = kolom.Length
+            daftar.Add(New List(Of Integer))
+            Dim angka3 As Integer = (angka2 - 1)
+            Dim j As Integer = 0
+            Do While (j <= angka3)
+                daftar.Item(i).Add(Convert.ToInt32(kolom(j)))
+                j += 1
+            Loop
+            If ((i <> 0) AndAlso (daftar.Item(i).Count <> daftar.Item((i - 1)).Count)) Then
+                Return Nothing
+            End If
+            i += 1
+        Loop
+        Return daftar
+    End Function
+
+    Private Sub cetakHasil(ByVal matrix As List(Of List(Of Integer)))
+        Dim hasil As String = ""
+        Dim angka As Integer = (matrix.Count - 1)
+        Dim i As Integer = 0
+        Do While (i <= angka)
+            Dim angka2 As Integer = (matrix.Item(i).Count - 1)
+            Dim j As Integer = 0
+            Do While (j <= angka2)
+                hasil = (hasil & Convert.ToString(matrix.Item(i).Item(j)) & vbTab)
+                j += 1
+            Loop
+            hasil = (hasil & vbCrLf)
+            i += 1
+        Loop
+        Me.hasilText.Text = hasil
+    End Sub
+
+
+    Private Function tambah(ByVal matrix1 As List(Of List(Of Integer)), ByVal matrix2 As List(Of List(Of Integer))) As List(Of List(Of Integer))
+        Dim angka As Integer = (matrix1.Count - 1)
+        Dim i As Integer = 0
+        Do While (i <= angka)
+            Dim angka2 As Integer = (matrix1.Item(i).Count - 1)
+            Dim j As Integer = 0
+            Do While (j <= angka2)
+                Dim daftar As List(Of Integer) = matrix1.Item(i)
+                daftar.Item(j) = (daftar.Item(j) + matrix2.Item(i).Item(j))
+                j += 1
+            Loop
+            i += 1
+        Loop
+        Return matrix1
+    End Function
+
+    Private Function kurang(ByVal matrix1 As List(Of List(Of Integer)), ByVal matrix2 As List(Of List(Of Integer))) As List(Of List(Of Integer))
+        Dim angka As Integer = (matrix1.Count - 1)
+        Dim i As Integer = 0
+        Do While (i <= angka)
+            Dim angka2 As Integer = (matrix1.Item(i).Count - 1)
+            Dim j As Integer = 0
+            Do While (j <= angka2)
+                Dim daftar As List(Of Integer) = matrix1.Item(i)
+                daftar.Item(j) = (daftar.Item(j) - matrix2.Item(i).Item(j))
+                j += 1
+            Loop
+            i += 1
+        Loop
+        Return matrix1
+    End Function
+
+
+
+    Private Function sesuai(ByVal matrix1 As List(Of List(Of Integer)), ByVal matrix2 As List(Of List(Of Integer))) As Boolean
+        Return ((matrix1.Count = matrix2.Count) AndAlso (matrix1.Item(0).Count = matrix2.Item(0).Count))
+    End Function
+
+    Private Sub hitungButton_Click(sender As Object, e As EventArgs) Handles hitungButton.Click
+        Dim daftarMatriks1 As List(Of List(Of Integer)) = Me.masukan(Me.matriks1.Text)
+        Dim daftarMatriks2 As List(Of List(Of Integer)) = Me.masukan(Me.matriks2.Text)
+        If ((Not daftarMatriks1 Is Nothing) And (Not daftarMatriks2 Is Nothing)) Then
+            If Me.sesuai(daftarMatriks1, daftarMatriks2) Then
+                Select Case Convert.ToString(Me.operasi.SelectedItem)
+                    Case "Tambah"
+                        daftarMatriks1 = Me.tambah(daftarMatriks1, daftarMatriks2)
+                        Me.cetakHasil(daftarMatriks1)
+                        Exit Select
+                    Case "Kurang"
+                        daftarMatriks1 = Me.kurang(daftarMatriks1, daftarMatriks2)
+                        Me.cetakHasil(daftarMatriks1)
+                        Exit Select
+                End Select
+            Else
+                MessageBox.Show("Jumlah baris dan kolom untuk kedua matriks harus sama!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Hand)
+            End If
+        Else
+            MessageBox.Show("Bukan matriks yang valid", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Hand)
+        End If
+
+    End Sub
 
 End Class
